c3d70945d048205dc64664edf4cf3faa22631ac4
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-06-29 20:12:54 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -191,7 +191,7 @@ async def cb_handler(client, query: CallbackQuery):
             disable_web_page_preview=True
         )
 
-     elif data == "commands":
+    elif data == "commands":
         await query.message.edit_text(
             text=Txt.HELP_TXT.format(client.mention),
             reply_markup=InlineKeyboardMarkup([
