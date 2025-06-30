ac6061e9d0a25b0d9365839aafefa84be4b375da
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-06-30 13:32:00 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -127,22 +127,7 @@ async def cb_handler(client, query: CallbackQuery):
         ])
     )
 
-    elif data == "mode":
-        current_mode = await codeflixbots.get_rename_mode(user_id)
-        auto_tick = "✅" if current_mode == "auto" else ""
-        manual_tick = "✅" if current_mode == "manual" else ""
-
-    await query.message.edit_text(
-        "Choose your renaming mode:",
-        reply_markup=InlineKeyboardMarkup([
-            [
-                InlineKeyboardButton(f"Auto Rename {auto_tick}", callback_data="set_auto"),
-                InlineKeyboardButton(f"Manual Rename {manual_tick}", callback_data="set_manual")
-            ],
-            [InlineKeyboardButton("« Back", callback_data="help")]
-        ])
-    )
-
+    
 elif data in ["set_auto", "set_manual"]:
     mode = "auto" if data == "set_auto" else "manual"
     await codeflixbots.set_rename_mode(user_id, mode)
