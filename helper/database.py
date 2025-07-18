f90348c64619658c81d5d2e9e97543d54141d5c0
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-07-18 18:58:47 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -13,34 +13,30 @@ class Database:
         except Exception as e:
             logging.error(f"Failed to connect to MongoDB: {e}")
             raise e  # Re-raise the exception after logging it
-        self.codeflixbots = self._client[database_name]
-        self.col = self.codeflixbots.user
+        self.Botskingdom = self._client[database_name]
+        self.col = self.Botskingdom.user
 
-    def new_user(self, id):
+    def new_user(self, id, username=None):
         return dict(
             _id=int(id),
+            username=username.lower() if username else None,
             join_date=datetime.date.today().isoformat(),
             file_id=None,
             caption=None,
             metadata=True,
-            metadata_code="Telegram : @codeflixbots",
+            metadata_code="Telegram : @botskingdoms",
             format_template=None,
             ban_status=dict(
                 is_banned=False,
-                ban_duration=0,
-                banned_on=datetime.date.max.isoformat(),
-                ban_reason=''
-            )
-        )
-
+@@ -37,7 +36,7 @@ def new_user(self, id, username=None):
     async def add_user(self, b, m):
         u = m.from_user
         if not await self.is_user_exist(u.id):
-            user = self.new_user(u.id)
+            user = self.new_user(u.id, u.username)
             try:
                 await self.col.insert_one(user)
                 await send_log(b, u)
-            except Exception as e:
+
                 logging.error(f"Error adding user {u.id}: {e}")
 
     async def is_user_exist(self, id):
@@ -139,62 +135,67 @@ class Database:
 
     async def set_metadata(self, user_id, metadata):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'metadata': metadata}})
+        
+@@ -143,74 +142,59 @@ async def set_metadata(self, user_id, metadata):
 
     async def get_title(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('title', 'Anime world')
+        return user.get('title', 'Bots kingdom')
 
     async def set_title(self, user_id, title):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'title': title}})
 
     async def get_author(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('author', 'Anime world')
+        return user.get('author', 'Botskingdoms')
 
     async def set_author(self, user_id, author):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'author': author}})
 
     async def get_artist(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('artist', 'Anime world')
+        return user.get('artist', 'Botskingdoms')
 
     async def set_artist(self, user_id, artist):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'artist': artist}})
 
     async def get_audio(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('audio', 'Anime world')
+        return user.get('audio', 'Bots kingdom')
 
     async def set_audio(self, user_id, audio):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'audio': audio}})
 
     async def get_subtitle(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('subtitle', "Anime world")
+        return user.get('subtitle', "Botskingdoms")
 
     async def set_subtitle(self, user_id, subtitle):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'subtitle': subtitle}})
 
     async def get_video(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('video', 'Animeworld')
+        return user.get('video', 'Botskingdoms')
 
     async def set_video(self, user_id, video):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'video': video}})
 
     async def get_encoded_by(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('encoded_by', "Anime world")
+        return user.get('encoded_by', "Botskingdoms")
 
     async def set_encoded_by(self, user_id, encoded_by):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'encoded_by': encoded_by}})
-        
+
     async def get_custom_tag(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('customtag', "Anime world")
+        return user.get('customtag', "Botskingdoms")
 
     async def set_custom_tag(self, user_id, custom_tag):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'custom_tag': custom_tag}})
 
+   
 
-codeflixbots = Database(Config.DB_URL, Config.DB_NAME)
+
+
+Botskingdom = Database(Config.DB_URL, Config.DB_NAME)
