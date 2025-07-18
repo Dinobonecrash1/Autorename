5cf9cf3e8d17cf5f8ae68c85f36ee5461cf10c2a
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-07-18 18:41:00 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -2,28 +2,8 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from helper.database import Botskingdom
 from config import Config
-from functools import wraps
-
-ADMIN_URL = Config.ADMIN_URL
-
-def check_ban(func):
-    @wraps(func)
-    async def wrapper(client, message, *args, **kwargs):
-        user_id = message.from_user.id
-        user = await Botskingdom.col.find_one({"_id": user_id})
-        if user and user.get("ban_status", {}).get("is_banned", False):
-            keyboard = InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ...!!", url=ADMIN_URL)]]
-            )
-            return await message.reply_text(
-                "Wᴛғ ʏᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ ғʀᴏᴍ ᴜsɪɴɢ ᴍᴇ ʙʏ ᴏᴜʀ ᴀᴅᴍɪɴ/ᴏᴡɴᴇʀ . Iғ ʏᴏᴜ ᴛʜɪɴᴋs ɪᴛ's ᴍɪsᴛᴀᴋᴇ ᴄʟɪᴄᴋ ᴏɴ **ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ...!!**",
-                reply_markup=keyboard
-            )
-        return await func(client, message, *args, **kwargs)
-    return wrapper
 
 @Client.on_message(filters.private & filters.command('set_caption'))
-@check_ban
 async def add_caption(client, message):
     if len(message.command) == 1:
        return await message.reply_text("**Give The Caption\n\nExample :- `/set_caption 📕Name ➠ : {filename} \n\n🔗 Size ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}`**")
@@ -32,7 +12,6 @@ async def add_caption(client, message):
     await message.reply_text("**Your Caption Successfully Added ✅**")
    
 @Client.on_message(filters.private & filters.command('del_caption'))
-@check_ban
 async def delete_caption(client, message):
     caption = await Botskingdom.get_caption(message.from_user.id)  
     if not caption:
@@ -41,7 +20,6 @@ async def delete_caption(client, message):
     await message.reply_text("**Your Caption Successfully Deleted 🗑️**")
                                        
 @Client.on_message(filters.private & filters.command(['see_caption', 'view_caption']))
-@check_ban
 async def see_caption(client, message):
     caption = await Botskingdom.get_caption(message.from_user.id)  
     if caption:
@@ -51,7 +29,6 @@ async def see_caption(client, message):
 
 
 @Client.on_message(filters.private & filters.command(['view_thumb', 'viewthumb']))
-@check_ban
 async def viewthumb(client, message):    
     thumb = await Botskingdom.get_thumbnail(message.from_user.id)
     if thumb:
@@ -60,13 +37,11 @@ async def viewthumb(client, message):
         await message.reply_text("**You Don't Have Any Thumbnail ❌**") 
 		
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
-@check_ban
 async def removethumb(client, message):
     await Botskingdom.set_thumbnail(message.from_user.id, file_id=None)
     await message.reply_text("**Thumbnail Deleted Successfully 🗑️**")
 	
 @Client.on_message(filters.private & filters.photo)
-@check_ban
 async def addthumbs(client, message):
     mkn = await message.reply_text("Please Wait ...")
     await Botskingdom.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
